0173310a8fc6df804b76dd55b37c47e7a11c00e0
tarabarnett <taralynnbarnett00@gmail.com>
2024-03-14 10:10:17 -0400

    V0.8 testing for release 1

--- main_test.py
+++ main_test.py
@@ -5,44 +5,29 @@ from main import scoreTrack
 
 class MyTestCase(unittest.TestCase):
 
-    # tests to see if name is returned correctly
-    def test_init_name(self):
-        x = users('Alice')
-        self.assertIs(x.name, 'Alice')  # add assertion here
-
     # tests to see if string is returned correctly
     def test_print_info(self):
-        x = scoreTrack("Alice")
-        self.__letters = "HORSE"
-        self.assertEqual(scoreTrack(self.print_info(x)), "Alice spelled HORSE")
+        x = users("Alice")
+        self.assertEqual(x.print_info(), "Alice spelled: ")
 
     # tests for scoreTrack class
-    def test_str_not_letters(self):
-        self.__letters = None
-        self.__name = "Bob"
-        self.assertIs(self.__str__(), "Bob has no letters yet")
+    # tests to see if name is returned correctly
+    def test_name(self):
+        x = scoreTrack('Alice')
+        self.assertIs(x.name, 'Alice')  # add assertion here
 
     def test_str_letters(self):
-        self.__letters = "HOR"
-        self.__name = ("Alice")
-        self.assertEqual(self.__str__(), "Alice has the letters: HOR")
+        x = scoreTrack("Bob")
+        self.assertEqual(x.__str__(), "Bob has no letters yet")
 
     def test_letters(self):
-        x = "HORS"
-        self.assertIs(scoreTrack.letters, x)
+        x = scoreTrack("Alice")
+        self.assertEqual(x.letters, "")
 
-    def test_checkCondition1(self):
-        letters = "HOR"
-        self.assertNotEqual(scoreTrack.checkCondition(letters), "HORSE")
+    def test_checkCondition(self):
+        x = scoreTrack("Bob")
+        self.assertNotEqual(x.checkCondition(), "HORSE")
 
-    def test_checkCondition2(self):
-        letters = "HORSE"
-        self.assertEqual(scoreTrack.checkCondition(letters), "HORSE")
-
-    def test_add_letter1(self):
-        x = "HOR"
-        self.assertFalse(x, scoreTrack.add_letter)
-
-    def test_add_letter2(self):
-        x = "HORSE"
-        self.assertTrue(x, scoreTrack.add_letter)
+    def test_add_letter(self):
+        x = scoreTrack("HOR")
+        self.assertTrue(x.add_letter, "HORS")
